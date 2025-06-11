3ae47fdbed0cb21d705b4fae63bc2c27ad6dbc91
Ewa Krawczyk <75725771+ewa-krawczyk@users.noreply.github.com>
2025-06-11 18:03:51 +0200

    added new test clickingOnEmptyCartShouldDisplayEmptyCartMessage

--- src/test/java/pageObjects/MainPage.java
+++ src/test/java/pageObjects/MainPage.java
@@ -1,6 +1,13 @@
 package pageObjects;
 
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class MainPage extends pageObjects.BasePage {
     public final pageObjects.StoreHeaderComponent storeHeader;
@@ -13,5 +20,13 @@ public class MainPage extends pageObjects.BasePage {
     public MainPage go() {
         driver.get(baseURL);
         return this;
+    }
+
+    public void validateMessageOnEmptyCart() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement messageText = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector(".wc-block-mini-cart__empty-cart-wrapper p strong")));
+        messageText.getText();
+        Assertions.assertEquals("Your cart is currently empty!", messageText.getText(), "The message is not correct");
     }
 }
